d869b91759232f3a727136593d150b280cbd07f1
Nesrux <joaomarcosdevs@gmail.com>
2025-04-12 13:26:38 -0300

    maximo de ums consecutivos

--- Algoritimos_expert/02- arrays/Maximo_uns_consecutivos.java
+++ Algoritimos_expert/02- arrays/Maximo_uns_consecutivos.java
@@ -1,17 +1,24 @@
+/**
+ * LEETCODE Máximo de 1s consecutivos
+ */
+
 // Dado um array binário nums, retorne número máximo de 1s consecutivos no array
 
 //exemplo entrada  nums = [1,1,0,1, 0, 0, 1, 1 , 1]
 
 public class Maximo_uns_consecutivos {
     public static void main(String[] args) {
-        int[] nums = { 1, 1, 0, 1, 0, 0, 1, 1, 1 };
-        System.out.println(maxNums(nums));
+        int[] entrada_um = { 1, 1, 0, 1, 0, 0, 1, 1, 1 };
+        int[] entrada_dois = { 1, 0, 1, 1, 0, 1 };
+
+        System.out.println(maxNums(entrada_um));
+        System.out.println(maxNums(entrada_dois));
     }
 
     public static Integer maxNums(int[] nums) {
-        int maxOnes = 0; //Maior sequencia de 1's registrada
+        int maxOnes = 0; // Maior sequencia de 1's registrada
         int countOnes = 0; // Sequencia de 1s atual
-        
+
         for (int num : nums) {
             if (num == 1) {
                 countOnes++;
@@ -20,10 +27,14 @@ public class Maximo_uns_consecutivos {
                 if (countOnes > maxOnes) {
                     maxOnes = countOnes;
                 }
-                countOnes = 0; 
+                countOnes = 0;
             }
         }
 
-        return countOnes;
+        if (countOnes > maxOnes) {
+            maxOnes = countOnes;
+        }
+
+        return maxOnes;
     }
 }
