2785962738c985e9344ab876517a6a73953dea21
Nesrux <joaomarcosdevs@gmail.com>
2025-04-19 12:53:35 -0300

    recursividade de cauda

--- Algoritimos_expert/Fatorial_recursividade_de_cauda.java
+++ Algoritimos_expert/Fatorial_recursividade_de_cauda.java
@@ -1,5 +1,20 @@
-public class Fatorial_recursividade_de_calda {
+/*
+ * Ideia da solução com recursividade de cauuda:
+ * intermediário como um parâmentro adicional da função.
+ */
+public class Fatorial_recursividade_de_cauda {
 
-    
+    public static int fatorial(int i) {
+        return fatorialTailRecursive(i, 1);
+    }
+
+    public static int fatorialTailRecursive(int n, int total) {
+        if (n == 0) return total;
+        return fatorialTailRecursive(n - 1, n * total);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fatorial(5));
+    }
 
 }
