298861045ef7971ac46050ee8e1dd30978a47649
Nesrux <joaomarcosdevs@gmail.com>
2025-04-19 13:32:44 -0300

    fibonacci linear

--- Algoritimos_expert/03 - recursividade/Fibonacci.java
+++ Algoritimos_expert/03 - recursividade/Fibonacci.java
@@ -6,12 +6,25 @@ public class Fibonacci {
      * da sequência de fibonacci
      */
     // Solução ineficiente
-    public static int sequence(int i) {
-        if (i <= 1) return i;
-        return sequence(i-1) + sequence(i -2);
+    public static int fib_not_tail_recursive(int i) {
+        if (i <= 1)
+            return i;
+        return fib_not_tail_recursive(i - 1) + fib_not_tail_recursive(i - 2);
+    }
+
+    public static int fib(int n) {
+        return fibTailRecursive(n, 0, 1);
+    }
+    // Fibonacci Eficiente em O (N)
+    public static int fibTailRecursive(int n, int l, int r) {
+        if (n == 0) return l;
+        if (n == 1) return r;
+
+        return fibTailRecursive(n - 1, r, l + r);
     }
 
     public static void main(String[] args) {
-        System.out.println(sequence(5));
+        System.out.println(fib_not_tail_recursive(5)); // Não eficiente
+        System.out.println(fib(45)); // Eficiente
     }
 }
