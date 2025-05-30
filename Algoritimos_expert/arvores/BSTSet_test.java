206e8b47e2cd9f4a14a6c7d8bb38649721647a94
Nesrux <joaomarcosdevs@gmail.com>
2025-05-30 17:41:02 -0300

    method add

--- Algoritimos_expert/arvores/BSTSet_test.java
+++ Algoritimos_expert/arvores/BSTSet_test.java
@@ -4,9 +4,13 @@ import arvores.BSTree.BinarySearchTreeSet;
 
 public class BSTSet_test {
     public static void main(String[] args) {
-        var a = new BinarySearchTreeSet<Integer>();
-        System.out.println("Size: " + a.size());
-        System.out.println("IsEmpty: " + a.isEmpty());
+        var bst = new BinarySearchTreeSet<Integer>();
+        System.out.println("Size: " + bst.size());
+        System.out.println("IsEmpty: " + bst.isEmpty());
+    
+        bst.add(11);
+       
+
 
     }
 
--- Algoritimos_expert/arvores/BSTree/BinarySearchTreeSet.java
+++ Algoritimos_expert/arvores/BSTree/BinarySearchTreeSet.java
@@ -1,11 +1,13 @@
 package arvores.BSTree;
 
-public class BinarySearchTreeSet<K> {
+import java.util.Objects;
+
+public class BinarySearchTreeSet<K extends Comparable<K>> {
 
     private Node root;
     private int size;
 
-   public BinarySearchTreeSet() {
+    public BinarySearchTreeSet() {
         this.size = 0;
         this.root = new Node(null, null);
     }
@@ -16,6 +18,45 @@ public class BinarySearchTreeSet<K> {
 
     public boolean isEmpty() {
         return this.size == 0;
+    }
+
+    public void add(K key) {
+        Objects.requireNonNull(key, "key not be null");
+        if (this.isEmpty()) {
+            root = new Node(key, null);
+            root.left = new Node(null, root);
+            root.right = new Node(null, root);
+            size++;
+            return;
+        }
+        Node node = findKeyLocation(root, key);
+        if (node.isSentinel()) {
+            Node parent = node.parent;
+            Node newNode = new Node(key, parent);
+            newNode.left = new Node(null, newNode);
+            newNode.right = new Node(null, newNode);
+
+            if (node == parent.left) {
+                parent.left = newNode;
+            } else if (node == parent.right) {
+                parent.right = newNode;
+            }
+            this.size++;
+        }
+    }
+
+    private Node findKeyLocation(Node node, K key) {
+        while (!node.isSentinel()) {
+            int cmp = key.compareTo(node.key);
+            if (cmp == 0) {
+                return node;
+            } else if (cmp < 0) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+        return node;
     }
 
     private class Node {
@@ -32,4 +73,5 @@ public class BinarySearchTreeSet<K> {
             return key == null;
         }
     }
+
 }
